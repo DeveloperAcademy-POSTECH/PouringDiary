9c01fa013d85f5a4b4be9a866f2af22a3c815d4b
Kang Hee Jong <2010236+DevKang@users.noreply.github.com>
2022-08-10 05:21:58 +0900

    [add] add scripts for check plist write

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -8,3 +8,7 @@
 
 
 echo ${CI_FB_PLIST} >> ../Shared/Environment/GoogleService-Info.plist
+echo "Firebase GoogleService-Info.plist created at"
+cd ../Shared/Environment/
+echo pwd
+echo cat GoogleService-Info.plist
